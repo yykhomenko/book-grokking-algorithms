9829d2ff0605d92a7ff7a5c1ea1bd645d6bfbf0a
yykhomenko <yykhomenko@gmail.com>
2020-06-24 21:58:45 +0300

    write 4.4 bsearch

---
+++ сh4-fastsort/ex4.4.go
@@ -0,0 +1,34 @@
+package ch4_fastsort
+
+func binarySearch(arr []int, element int) int {
+	return bSearch(arr, element, 0, len(arr)-1)
+}
+
+func bSearch(arr []int, item, low, high int) int {
+
+	if len(arr) == 0 {
+		return -1
+	}
+
+	// if low == high {
+	// 	if arr[low] == item {
+	// 		return low
+	// 	} else {
+	// 		return -1
+	// 	}
+	// }
+
+	mid := (high - low) / 2
+	guess := arr[mid]
+
+	switch {
+	case guess == item:
+		return mid
+	case guess > item:
+		high = mid - 1
+		return bSearch(arr, item, low, high)
+	default:
+		low := mid + 1
+		return bSearch(arr, item, low, high)
+	}
+}

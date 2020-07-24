c84307d7d19e140cc7113c38e42d45b9fb2463fc
yykhomenko <yykhomenko@gmail.com>
2020-07-24 15:33:34 +0300

    added more tests

--- сh4-quicksort/qsort_test.go
+++ сh4-quicksort/qsort_test.go
@@ -5,13 +5,17 @@ import (
 	"testing"
 )
 
-func Test_qsort(t *testing.T) {
+func TestQsort(t *testing.T) {
 	tests := []struct {
 		name string
 		args []int
 		want []int
 	}{
-		{name: "valid", args: []int{3, 5, 2, 1, 4}, want: []int{1, 2, 3, 4, 5}},
+		{name: "empty", args: []int{}, want: []int{}},
+		{name: "one", args: []int{1}, want: []int{1}},
+		{name: "two", args: []int{2, 1}, want: []int{1, 2}},
+		{name: "some", args: []int{3, 5, 2, 1, 4}, want: []int{1, 2, 3, 4, 5}},
+		{name: "more", args: []int{-1, 50, -6, 80, 90, 11, 14, 17, -20, 23, 24}, want: []int{-20, -6, -1, 11, 14, 17, 23, 24, 50, 80, 90}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
